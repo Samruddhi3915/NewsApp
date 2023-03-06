b10a3fe23f175b907cae540d702c57df36dc6721
Samruddhi3915 <samruddhi3915@gmail.com>
2023-03-06 13:17:17 +0530

    My NewsApp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  c = "Sam";
+  render() {
+    return <div>{this.c}</div>;
+  }
+}
+
+export default App;
