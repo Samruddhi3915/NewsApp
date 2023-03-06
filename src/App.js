fe4e5bc8e09725d545d5c5a3e4314853987d4de1
Samruddhi3915 <samruddhi3915@gmail.com>
2023-03-06 14:51:00 +0530

    Basic component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-
+import Navbar from "./components/Navbar";
+import News from "./components/News";
 import React, { Component } from "react";
 
 class App extends Component {
   c = "Sam";
   render() {
-    return <div>{this.c}</div>;
+    return (
+      <div>
+        <Navbar />
+        <News />
+      </div>
+    );
   }
 }
 
